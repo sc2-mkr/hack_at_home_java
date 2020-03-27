3b7e4f5d106b003102459fb5fef2be3e9be2f129
Iregon <0iregon0@gmail.com>
2020-03-27 20:20:31 +0100

    Blank Spring Web project

---
+++ src/main/java/com/sc2/hackathome/java/HackAtHomeBeApplication.java
@@ -0,0 +1,13 @@
+package com.sc2.hackathome.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackAtHomeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackAtHomeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc2/hackathome/java/HackAtHomeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc2.hackathome.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackAtHomeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
