b87924c8dce02312480d577fc8c525eb9b767a8c
PlayerUnknown737 <python.can.fly@gmail.com>
2020-03-28 12:05:13 +0100

    add user class

--- src/main/java/com/sc2/hackathome/java/user/User.java
+++ src/main/java/com/sc2/hackathome/java/user/User.java
@@ -1,4 +1,37 @@
 package com.sc2.hackathome.java.user;
 
 public class User {
+    private final long id;
+    private final String email;
+    private final String name;
+    private final String surname;
+    private final String password;
+
+    public User(long id, String username, String name, String surname, String password) {
+        this.id = id;
+        this.email = username;
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
