ff8f3c7a238c9032cd94ca1354f206860156622e
Iregon <0iregon0@gmail.com>
2020-03-29 03:15:42 +0200

    Add Customer API

---
+++ src/main/java/com/sc2/hackathome/customer/Customer.java
@@ -0,0 +1,30 @@
+package com.sc2.hackathome.customer;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "CUSTOMER")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @Column(name = "ADDRESS")
+    @NotNull
+    private String address;
+
+    @Column(name = "PHONE_NUMBER")
+    @NotNull
+    private String phoneNumber;
+}
