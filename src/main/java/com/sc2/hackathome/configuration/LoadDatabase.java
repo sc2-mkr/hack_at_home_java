f9cbda4631c24d351b1004cb4643134e21d63470
Iregon <0iregon0@gmail.com>
2020-03-29 11:07:03 +0200

    Bugfix

--- src/main/java/com/sc2/hackathome/configuration/LoadDatabase.java
+++ src/main/java/com/sc2/hackathome/configuration/LoadDatabase.java
@@ -11,14 +11,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class LoadDatabase {
 
-    @Bean
-    CommandLineRunner initDatabase(DeliverymanRepository deliverymanRepository,
-                                   ShippingListRepository shippingListRepository) {
-        return args -> {
-            deliverymanRepository.save(new Deliveryman(1, "Iregon", "Alessandro", "Tornesello", "123456"));
-            deliverymanRepository.save(new Deliveryman(2, "Deliveryman#2", "Pinco", "Pallino", "123456"));
-            deliverymanRepository.save(new Deliveryman(3, "admin", "Alessandro", "Tornesello", "admin"));
-            shippingListRepository.save(new ShippingList(1,2,1));
-        };
-    }
+//    @Bean
+//    CommandLineRunner initDatabase(DeliverymanRepository deliverymanRepository,
+//                                   ShippingListRepository shippingListRepository) {
+//        return args -> {
+//            deliverymanRepository.save(new Deliveryman(1, "Iregon", "Alessandro", "Tornesello", "123456"));
+//            deliverymanRepository.save(new Deliveryman(2, "Deliveryman#2", "Pinco", "Pallino", "123456"));
+//            deliverymanRepository.save(new Deliveryman(3, "admin", "Alessandro", "Tornesello", "admin"));
+//            shippingListRepository.save(new ShippingList(1,2,1));
+//        };
+//    }
 }
