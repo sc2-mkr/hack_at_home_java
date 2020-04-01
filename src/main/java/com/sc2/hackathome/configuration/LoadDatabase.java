d10532ffaed0b742d20787d5631e69e24538e9f3
Iregon <0iregon0@gmail.com>
2020-04-01 22:25:42 +0200

    Remove initialization of DB

--- src/main/java/com/sc2/hackathome/configuration/LoadDatabase.java
+++ src/main/java/com/sc2/hackathome/configuration/LoadDatabase.java
@@ -21,22 +21,22 @@ class LoadDatabase {
     CommandLineRunner initDatabase(DeliverymanRepository deliverymanRepository,
                                    ShippingListRepository shippingListRepository) {
         return args -> {
-            deliverymanRepository.save(new Deliveryman(1, "Iregon", "Alessandro", "Tornesello"));
-            deliverymanRepository.save(new Deliveryman(2, "Deliveryman#2", "Pinco", "Pallino"));
-            deliverymanRepository.save(new Deliveryman(3, "admin", "Alessandro", "Tornesello"));
-
-            List<ShippingItem> list1 = new ArrayList<>();
-            list1.add(new ShippingItem(1, "3 confezioni di pasta"));
-
-            List<ShippingItem> list2 = new ArrayList<>();
-            list2.add(new ShippingItem(1, "1 scatoletta di tonno"));
-
-            List<ShippingItem> list3 = new ArrayList<>();
-            list3.add(new ShippingItem(1, "300g di macinato"));
-
-            shippingListRepository.save(new ShippingList(1, list1, 2, null, "Via Maria 13", "Lecce"));
-            shippingListRepository.save(new ShippingList(1, list2, 1, null, "Via Piemonte 43", "Matino"));
-            shippingListRepository.save(new ShippingList(1, list3, 1, null, "Via Roma 88", "Collepasso"));
+//            deliverymanRepository.save(new Deliveryman(1, "Iregon", "Alessandro", "Tornesello"));
+//            deliverymanRepository.save(new Deliveryman(2, "Deliveryman#2", "Pinco", "Pallino"));
+//            deliverymanRepository.save(new Deliveryman(3, "admin", "Alessandro", "Tornesello"));
+//
+//            List<ShippingItem> list1 = new ArrayList<>();
+//            list1.add(new ShippingItem(1, "3 confezioni di pasta"));
+//
+//            List<ShippingItem> list2 = new ArrayList<>();
+//            list2.add(new ShippingItem(1, "1 scatoletta di tonno"));
+//
+//            List<ShippingItem> list3 = new ArrayList<>();
+//            list3.add(new ShippingItem(1, "300g di macinato"));
+//
+//            shippingListRepository.save(new ShippingList(1, list1, 2, null, "Via Maria 13", "Lecce"));
+//            shippingListRepository.save(new ShippingList(1, list2, 1, null, "Via Piemonte 43", "Matino"));
+//            shippingListRepository.save(new ShippingList(1, list3, 1, null, "Via Roma 88", "Collepasso"));
 
         };
     }
